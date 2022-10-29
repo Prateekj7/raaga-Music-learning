abe2bd118998bd3e52af85996379b82a0c5d018f
abhiroopdutta <abhiroopd@ymail.com>
2022-10-29 13:56:10 +0530

    Featured artist card takes img as parameter

--- frontend/src/components/FeaturedArtist/FeaturedArtistCard.js
+++ frontend/src/components/FeaturedArtist/FeaturedArtistCard.js
@@ -1,13 +1,13 @@
 import styles from "./FeaturedArtistCard.module.css";
 import React from "react";
 
-function FeaturedArtistCard({ showFooter }) {
+function FeaturedArtistCard({ showFooter, imgUrl = "https://www.pngitem.com/pimgs/m/135-1356850_blossom-powerpuff-girls-png-transparent-background-powerpuff-girls.png" }) {
 
     return (
         <div className={`${styles["card"]}`}>
             <div className={`${styles["Stroke-28"]}`}>
                 <div className={`${styles["card_img"]}`}>
-                    <img src="https://www.pngitem.com/pimgs/m/135-1356850_blossom-powerpuff-girls-png-transparent-background-powerpuff-girls.png"
+                    <img src={imgUrl}
                         alt="user-image" />
                 </div>
             </div>
