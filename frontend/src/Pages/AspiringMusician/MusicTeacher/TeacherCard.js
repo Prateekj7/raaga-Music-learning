c3792d53a8ca8318f071b1242554f445e7027818
capmahin <mahinulhuque1996@gmail.com>
2022-10-31 13:46:52 +0600

    add login-button:active in TeacherCard review button

--- frontend/src/Pages/AspiringMusician/MusicTeacher/TeacherCard.js
+++ frontend/src/Pages/AspiringMusician/MusicTeacher/TeacherCard.js
@@ -6,9 +6,8 @@ import Review from "./Review";
 import ReadMore from "./ReadMore";
 import Collapse from "react-bootstrap/Collapse";
 import FeaturedArtistCard from "../../../components/FeaturedArtist/FeaturedArtistCard";
-import Accordion from 'react-bootstrap/Accordion';
+import Accordion from "react-bootstrap/Accordion";
 import ReactPlayer from "react-player";
-
 
 const TeacherCard = ({ teacher }) => {
   const { name, image_url, post, experience, hourly_rate } = teacher;
@@ -25,12 +24,14 @@ const TeacherCard = ({ teacher }) => {
     <div className={`my-2 px-3`}>
       <Row>
         <Col lg={2}>
-          <div >
+          <div>
             <FeaturedArtistCard showFooter={false} imgUrl={teacher.image_url} />
           </div>
         </Col>
         <Col lg={10}>
-          <div className={`${styles["testimonial-card-text"]} px-lg-4 pt-4 mb-3`}>
+          <div
+            className={`${styles["testimonial-card-text"]} px-lg-4 pt-4 mb-3`}
+          >
             <h4 className={`${styles["teacher-name"]}`}>{name}</h4>
             <div className={`${styles["teacher-experience"]}`}>
               {`${experience} years`} experience
@@ -45,19 +46,18 @@ const TeacherCard = ({ teacher }) => {
               <FaStar />
               <FaStar />
             </div>
-            <Button className={`${styles["login-button"]} me-2 mt-3`}>Reviews</Button>
+            <button className={`${styles["login-button"]} me-2 mt-3`}>
+              Reviews
+            </button>
           </div>
         </Col>
         <Accordion flush className="mb-3 p-lg-0 mx-lg-0">
           <Accordion.Item eventKey="0">
             <Accordion.Header className={`${styles["read-more-header"]}`}>
-              <div className={`${styles["read-more"]}`}>
-                Read More
-              </div></Accordion.Header>
+              <div className={`${styles["read-more"]}`}>Read More</div>
+            </Accordion.Header>
             <Accordion.Body className="mx-0 px-0">
-              <ReadMore
-                teacher={teacher}
-              ></ReadMore>
+              <ReadMore teacher={teacher}></ReadMore>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
