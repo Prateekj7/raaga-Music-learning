cc086e430dac5c2aaf7b9e44f1e2422daf91a872
capmahin <mahinulhuque1996@gmail.com>
2022-10-31 11:29:45 +0600

    Fix button style Fixes #9

--- frontend/src/Pages/AspiringMusician/MusicTeacher/Paginaiton.js
+++ frontend/src/Pages/AspiringMusician/MusicTeacher/Paginaiton.js
@@ -4,32 +4,34 @@ const Paginaiton = () => {
   return (
     <div>
       <nav aria-label="Page navigation example">
-        <ul className="pagination justify-content-center">
+        <ul className="pagination justify-content-center ">
           <li className="page-item">
             <a className="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-              <span className="sr-only">Previous</span>
+              <span aria-hidden="true" className="text-dark">
+                &laquo;
+              </span>
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#">
+            <a className="page-link text-dark" href="#">
               1
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#">
+            <a className="page-link text-dark" href="#">
               2
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#">
+            <a className="page-link text-dark" href="#">
               3
             </a>
           </li>
           <li className="page-item">
             <a className="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-              <span className="sr-only">Next</span>
+              <span aria-hidden="true" className="text-dark">
+                &raquo;
+              </span>
             </a>
           </li>
         </ul>
