7256432bd6e033fffd5899bbc37c0e663e579081
abhiroopdutta <abhiroopd@ymail.com>
2022-11-08 00:27:14 +0530

    Using react-hooks-form to simplify forms

--- frontend/src/Pages/TeacherProfile.js
+++ frontend/src/Pages/TeacherProfile.js
@@ -7,6 +7,7 @@ import singleArrowIcon from "../images/singleArrowIcon.svg";
 import FeaturedArtistCard from "../components/FeaturedArtist/FeaturedArtistCard";
 import { LoginContext } from "../LoginContext";
 import { FaEdit } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 
 function TeacherProfile() {
 
@@ -14,6 +15,7 @@ function TeacherProfile() {
     const [loggedInUser, setLoggedInUser] = loggedInUserContext;
     const [savedProfile, setSavedProfile] = useState();
     const [editProfileMode, setEditProfileMode] = useState(false);
+    const { register, handleSubmit, reset, formState: { isDirty, dirtyFields } } = useForm();
 
     useEffect(() => {
         let data = {
@@ -26,23 +28,24 @@ function TeacherProfile() {
             body: JSON.stringify(data),
         };
 
+        const testData = {
+            name: "Joe Satriani",
+            gender: "male",
+            city: "Duliajan",
+            state: "Assam",
+            pin_code: "211008",
+            about: "I am a multi talented Grammy award winning multi instrumentalist.",
+            qualification: "Berklee College of Music",
+            contact_number: "919954199108",
+            email_id: "jor@gmail.com",
+            achievement: "Grammy, Tony, Emmy, Academy",
+            experience: "25",
+        };
+
         const getTableData = async () => {
             try {
-                setSavedProfile(
-                    {
-                        name: "Joe Satriani",
-                        gender: "male",
-                        city: "Duliajan",
-                        state: "Assam",
-                        pin_code: "211008",
-                        about: "I am a multi talented Grammy award winning multi instrumentalist.",
-                        qualification: "Berklee College of Music",
-                        contact_number: "91-9954199108",
-                        email_id: "jor@gmail.com",
-                        achievement: "Grammy, Tony, Emmy, Academy",
-                        experience: "25",
-                    }
-                );
+                setSavedProfile(testData);
+                reset(testData);
                 const response = await fetch("/api/read_teacher_metadata/", requestOptions);
                 const result = await response.json();
                 if (response.ok) {
@@ -58,32 +61,35 @@ function TeacherProfile() {
         getTableData();
     }, [loggedInUser.id]);
 
-    const handleSaveProfile = (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const formDataObj = Object.fromEntries(formData.entries());
+    const handleSaveProfile = (formData) => {
         setEditProfileMode(false);
-        console.log(formDataObj);
-        const getTableData = async () => {
-            try {
-                setSavedProfile((oldState) => { return { ...oldState, ...formDataObj }; });
-                // const response = await fetch("/api/read_teacher_metadata/", requestOptions);
-                // const result = await response.json();
-                // if (response.ok) {
-                //     setSavedProfile(result[0]);
-                // } else {
-                //     throw Error(result);
-                // }
-            } catch (err) {
-                console.log(err.message);
-            }
-        };
+        if (!isDirty) {
+            return;
+        }
+        const dirtyForm = Object.keys(dirtyFields)
+            .reduce((finalData, key) => ({ ...finalData, [key]: formData[key] }), {});
+        setSavedProfile((oldState) => ({ ...oldState, ...dirtyForm }));
+        // const getTableData = async () => {
+        //     try {
+        //         setSavedProfile((oldState) => { return { ...oldState, ...formDataObj }; });
+        //         // const response = await fetch("/api/read_teacher_metadata/", requestOptions);
+        //         // const result = await response.json();
+        //         // if (response.ok) {
+        //         //     setSavedProfile(result[0]);
+        //         // } else {
+        //         //     throw Error(result);
+        //         // }
+        //     } catch (err) {
+        //         console.log(err.message);
+        //     }
+        // };
 
-        getTableData();
-        e.target.reset();
+        // getTableData();
     };
-    const handleCancelEditProfile = () => {
+    const handleCancelEditProfile = (e) => {
+        e.preventDefault();
         setEditProfileMode(false);
+        reset(savedProfile);
     };
 
     const handleEditProfile = () => {
@@ -111,15 +117,14 @@ function TeacherProfile() {
                     <FeaturedArtistCard showFooter={false} />
                 </Col>
                 <Col xs={12} md={9}>
-                    <Form onSubmit={handleSaveProfile} onReset={handleCancelEditProfile} id="signUpForm" className="mb-3">
+                    <Form onSubmit={handleSubmit(handleSaveProfile)} onReset={handleCancelEditProfile} id="signUpForm" className="mb-3">
                         <fieldset disabled={!editProfileMode}>
                             <Form.Group as={Row} className="mb-lg-3" controlId="formBasicName">
                                 <Form.Label column sm="2">Full Name</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none `}
-                                        name="name"
+                                        {...register("name")}
                                         type="text"
-                                        defaultValue={savedProfile?.name}
                                         maxLength={50}
                                     />
                                 </Col>
@@ -133,22 +138,19 @@ function TeacherProfile() {
                                                 className={`${styles["form-label"]} me-5`}
                                                 inline
                                                 label="Male"
-                                                name="gender"
+                                                {...register("gender")}
                                                 type={"radio"}
                                                 id={`male`}
                                                 value="male"
-                                                defaultChecked={(savedProfile?.gender === "male")}
-
                                             />
                                             <Form.Check
                                                 className={`${styles["form-label"]}`}
                                                 inline
                                                 label="Female"
-                                                name="gender"
+                                                {...register("gender")}
                                                 type={"radio"}
                                                 id={`female`}
                                                 value="female"
-                                                defaultChecked={savedProfile?.gender === "female"}
                                             />
                                         </div>
                                     </Col>
@@ -158,9 +160,8 @@ function TeacherProfile() {
                                 <Form.Label column sm="2">Email</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none`}
-                                        name="email"
+                                        {...register("email_id")}
                                         type="email"
-                                        defaultValue={savedProfile?.email_id}
                                     />
                                 </Col>
                             </Form.Group>
@@ -168,9 +169,8 @@ function TeacherProfile() {
                                 <Form.Label column sm="2" className={`${styles["form-label"]}`}>Contact</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none`}
-                                        name="phone"
+                                        {...register("contact_number")}
                                         type="tele"
-                                        defaultValue={savedProfile?.contact_number}
                                         pattern="[0-9]{7,15}"
                                         maxLength="15"
                                         title="Please enter a valid phone number"
@@ -181,9 +181,8 @@ function TeacherProfile() {
                                 <Form.Label column sm="2">City</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none `}
-                                        name="city"
+                                        {...register("city")}
                                         type="text"
-                                        defaultValue={savedProfile?.city}
                                         maxLength={50}
                                     />
                                 </Col>
@@ -192,9 +191,8 @@ function TeacherProfile() {
                                 <Form.Label column sm="2">State</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none `}
-                                        name="state"
+                                        {...register("state")}
                                         type="text"
-                                        defaultValue={savedProfile?.state}
                                         maxLength={50}
                                     />
                                 </Col>
@@ -203,9 +201,8 @@ function TeacherProfile() {
                                 <Form.Label column sm="2">Pincode</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none `}
-                                        name="pincode"
+                                        {...register("pin_code")}
                                         type="text"
-                                        defaultValue={savedProfile?.pin_code}
                                         maxLength={20}
                                     />
                                 </Col>
@@ -214,10 +211,9 @@ function TeacherProfile() {
                                 <Form.Label column sm="2">About</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none `}
-                                        name="about"
+                                        {...register("about")}
                                         as="textarea"
                                         rows={3}
-                                        defaultValue={savedProfile?.about}
                                         maxLength={500}
                                     />
                                 </Col>
@@ -226,9 +222,8 @@ function TeacherProfile() {
                                 <Form.Label column sm="2">Qualification</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none `}
-                                        name="qualification"
+                                        {...register("qualification")}
                                         type="text"
-                                        defaultValue={savedProfile?.qualification}
                                         maxLength={100}
                                     />
                                 </Col>
@@ -237,9 +232,8 @@ function TeacherProfile() {
                                 <Form.Label column sm="2">Achievement</Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none `}
-                                        name="achievement"
+                                        {...register("achievement")}
                                         type="text"
-                                        defaultValue={savedProfile?.achievement}
                                         maxLength={200}
                                     />
                                 </Col>
@@ -248,9 +242,8 @@ function TeacherProfile() {
                                 <Form.Label column sm="2">Experience (in years) </Form.Label>
                                 <Col>
                                     <Form.Control className={`${styles["form-number-input"]} shadow-none `}
-                                        name="experience"
+                                        {...register("experience")}
                                         type="number"
-                                        defaultValue={savedProfile?.experience}
                                         onWheel={(e) => e.target.blur()}
                                         min="0"
                                         max="100"
