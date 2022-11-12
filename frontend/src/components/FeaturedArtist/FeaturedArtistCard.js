0e88727c7e0a647efc362fe0f7330ccc3eff2369
abhiroopdutta <abhiroopd@ymail.com>
2022-11-12 21:35:25 +0530

    Added skeleton for featured artist card

--- frontend/src/components/FeaturedArtist/FeaturedArtistCard.js
+++ frontend/src/components/FeaturedArtist/FeaturedArtistCard.js
@@ -1,14 +1,20 @@
 import styles from "./FeaturedArtistCard.module.css";
 import React from "react";
 
-function FeaturedArtistCard({ showFooter, imgUrl = "https://www.pngitem.com/pimgs/m/135-1356850_blossom-powerpuff-girls-png-transparent-background-powerpuff-girls.png" }) {
+function FeaturedArtistCard({
+    showFooter,
+    imgUrl = "https://www.pngitem.com/pimgs/m/135-1356850_blossom-powerpuff-girls-png-transparent-background-powerpuff-girls.png",
+    skeleton
+}) {
 
     return (
         <div className={`${styles["card"]}`}>
             <div className={`${styles["Stroke-28"]}`}>
-                <div className={`${styles["card_img"]}`}>
-                    <img src={imgUrl}
-                        alt="user-image" />
+                <div className={`${styles["card_img"]} ${skeleton ? styles["skeleton"] : ""}`}>
+                    {skeleton ? null :
+                        <img src={imgUrl}
+                            alt="user-image" />
+                    }
                 </div>
             </div>
             {showFooter ? <div className={`${styles["card_info"]}`}>
