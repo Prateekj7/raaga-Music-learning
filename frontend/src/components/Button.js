cdc701e0c328eac2a99b95e84372ee0e2923949c
abhiroopdutta <abhiroopd@ymail.com>
2022-11-15 23:19:18 +0530

    Added selected prop for custom button

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -3,13 +3,13 @@ import styles from "./Button.module.css";
 import { Button } from "react-bootstrap";
 
 
-function RaagaButton({ text, skeleton, className, ...props }) {
+function RaagaButton({ text, skeleton, className, selected, ...props }) {
 
     return (
         <Button
             {...props}
             disabled={skeleton ? true : false}
-            className={`${skeleton ? styles["skeleton"] : styles["login-button"]} ${className}`}
+            className={`${skeleton ? styles["skeleton"] : styles["login-button"]} ${selected ? styles["selected"] : null} ${className}`}
         >
             {text}
         </Button>
