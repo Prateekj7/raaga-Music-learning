0c43b9d1f0b1abcd2352558e9cb3ca0e60e6a19c
abhiroopdutta <abhiroopd@ymail.com>
2022-11-16 21:00:26 +0530

    Notification props updated

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Toast from 'react-bootstrap/Toast';
 import ToastContainer from 'react-bootstrap/ToastContainer';
 
-function Notification({ show, setShow, message }) {
+function Notification({ show, setShow, message, position = "middle-center", containerPosition = "fixed" }) {
     return (
         <Row>
             <Col xs={6}>
-                <ToastContainer containerPosition="fixed" position="middle-center" className="p-3">
-                    <Toast bg="light" onClose={() => setShow({ show: false, message: "" })} show={show} delay={4500} autohide>
+                <ToastContainer containerPosition={containerPosition} position={position} className="p-3">
+                    <Toast onClose={() => setShow({ show: false, message: "" })} show={show} delay={4500} autohide>
                         <Toast.Header>
                             <strong className="me-auto">Raaga</strong>
                         </Toast.Header>
-                        <Toast.Body>{message}</Toast.Body>
+                        <Toast.Body >{message}</Toast.Body>
                     </Toast>
                 </ToastContainer>
             </Col>
