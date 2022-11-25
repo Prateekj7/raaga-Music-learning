23e6697d7776c1466060ade9aade6faace120d49
abhiroopdutta <abhiroopd@ymail.com>
2022-11-25 16:18:31 +0530

    Fixed pagination styles

--- frontend/src/components/Paginaiton.js
+++ frontend/src/components/Paginaiton.js
@@ -1,40 +1,28 @@
 import React from "react";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
+import styles from "./Pagination.module.css";
+import Pagination from 'react-bootstrap/Pagination';
 
 const Paginaiton = () => {
   return (
-    <div>
-      <nav aria-label="Page navigation example">
-        <ul className="pagination justify-content-center">
-          <li className="page-item">
-            <a className="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-              <span className="sr-only">Previous</span>
-            </a>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              1
-            </a>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              2
-            </a>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              3
-            </a>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-              <span className="sr-only">Next</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Pagination className={`${styles["pagination"]}`}>
+      <Pagination.Prev className="me-2">
+        <SlArrowLeft />
+      </Pagination.Prev>
+      <div className={`${styles["page-number-container"]} px-2 d-flex`}>
+        <Pagination.Item>{1}</Pagination.Item>
+        <Pagination.Item>{2}</Pagination.Item>
+        <Pagination.Item>{3}</Pagination.Item>
+        <Pagination.Item>{4}</Pagination.Item>
+        <Pagination.Item>{5}</Pagination.Item>
+        <Pagination.Item>{6}</Pagination.Item>
+        <Pagination.Item>{7}</Pagination.Item>
+        <Pagination.Item>{8}</Pagination.Item>
+      </div>
+      <Pagination.Next className="ms-2">
+        <SlArrowRight />
+      </Pagination.Next>
+    </Pagination>
   );
 };
 
